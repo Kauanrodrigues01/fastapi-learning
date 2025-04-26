1ee0c4d173711f7e1802aac0d0f23aa545e146bb
Kauanrodrigues01m <kauanrl09@gmail.com>
2025-04-26 19:22:29 -0300

    chore: update aula 07

--- Aula 07 - Refatorando e Estruturando o Projeto/fast_zero/app.py
+++ Aula 07 - Refatorando e Estruturando o Projeto/fast_zero/app.py
@@ -5,7 +5,7 @@ from fast_zero.routers import auth, users
 app = FastAPI(
     debug=True,
     title='Aula Sobre Refatoração e Estrutura',
-    description='Aprendendo a utilizar routers e reorganizar o projeto.',
+    description='Aprendendo a utilizar routers, annotated e reorganizar o projeto.',
     version='0.1.0',
 )
 app.include_router(users.router)
